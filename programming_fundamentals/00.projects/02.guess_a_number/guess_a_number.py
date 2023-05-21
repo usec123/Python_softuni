fe6788937f7694c87abba8e393e96d937e32bcad
usec123 <marcello_k@abv.bg>
2023-05-22 00:18:21 +0300

    Added guess_a_number.py

---
+++ programming_fundamentals/00.projects/02.guess_a_number/guess_a_number.py
@@ -0,0 +1,18 @@
+from random import randint
+
+NUMBER = randint(1,100)
+guess = None
+attempts = 0
+
+while guess != NUMBER:
+    try: guess = int(input('Guess the number (1-100): '))
+    except ValueError: 
+        print('You must enter a number!')
+        continue
+    attempts += 1
+    if guess > NUMBER: print('Too High!')
+    elif guess < NUMBER: print('Too Low!')
+    else:
+        print('You guessed it!')
+        print(f'Attempts: {attempts}')
+        break

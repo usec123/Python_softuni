9384969423cca6f2e518bcf08ee9fbe1500063ec
usec123 <marcello_k@abv.bg>
2023-05-22 00:04:53 +0300

    Added rock_paper_scissors.py code

---
+++ programming_fundamentals/00.projects/01.rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,18 @@
+import random
+
+while True:
+    options = ['r','p','s','e']
+    full_names = ['Rock','Paper','Scissors']
+    player_input = input('Choose [r]ock, [p]aper or [s]cissors: ').lower()
+    if player_input not in options: raise SystemExit('Invalid Input. Try again...') #exception
+    else:
+        if player_input == 'e': break
+        player_input = options.index(player_input)
+        computer_input = random.randint(0,2)
+        print(f'The computer chose {full_names[computer_input]}')
+        if computer_input==player_input: print('Draw!')
+        else:
+            player_input-=1
+            if player_input < 0: player_input = 2
+            if player_input == computer_input: print('Player wins!')
+            else: print('Computer wins!')
